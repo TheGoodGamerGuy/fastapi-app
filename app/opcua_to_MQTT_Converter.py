29cab324b32651bebb5f5e2872d178b52bdaab45
TGGG <65331833+TheGoodGamerGuy@users.noreply.github.com>
2025-01-26 12:54:01 +0200

    node_id to nodeid

--- app/opcua_to_MQTT_Converter.py
+++ app/opcua_to_MQTT_Converter.py
@@ -27,15 +27,15 @@ def read_opcua_data(opcua_client, mqtt_client):
     print("Connected to OPC UA server")
 
     while True:
-        for NodeId in read_selected_nodes():
+        for node_id in read_selected_nodes():
             try:
-                value = opcua_client.get_node(NodeId).get_value()
-                NodeId_short = NodeId.replace("ns=2;s=DB15.", "")
-                mqtt_payload = json.dumps({"NodeId": NodeId_short, "value": value})
+                value = opcua_client.get_node(node_id).get_value()
+                node_id_short = node_id.replace("ns=2;s=DB15.", "")
+                mqtt_payload = json.dumps({"node_id": node_id_short, "value": value})
                 mqtt_client.publish(MQTT_TOPIC, mqtt_payload)
-                print(f"Published: {NodeId_short} = {value}")
+                print(f"Published: {node_id_short} = {value}")
             except Exception as e:
-                print(f"Error reading {NodeId}: {e}")
+                print(f"Error reading {node_id}: {e}")
         read_interval = int(os.environ.get('READ_INTERVAL', DEFAULT_READ_INTERVAL))
         time.sleep(read_interval)
 
